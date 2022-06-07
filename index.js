5f581b45d0703422e14aa8eb93d94a6ce002193c
Chidinma Nwatu <62130713+Simplystina@users.noreply.github.com>
2022-06-07 07:55:22 +0000

    complete code for the guessing game

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const guessGame =()=>{
+  
   const username = prompt('Enter your username: ')
-  if(!username){
+  if(!username){ //checks if user enters a username
     console.log('Please enter a valid username')
+    return
   }
-  console.log('Welcome to stage 1', username)
-let stage = 1
+  
+  console.log()
+  let stage = 1
    let range = 2
-  while(true){
+  console.log('Welcome to stage', stage, username)
+  console.log()
+  
+  while(true){  //loops runs until user enters a wrong input
 
      console.log('Generate a number between 1 and ', range)
-    let hiddenNo = Math.floor(Math.random() * range) +1
-    console.log(hiddenNo,'hiddenNo')
-    const guessNumber = parseInt(prompt('Your Guess: '))
-    if(guessNumber === hiddenNo){
-         console.log("You're in stage", stage++, username)
-      range++
+    let hiddenNo = Math.floor(Math.random() * range) +1 //generates number within specified range
+   
+    const guessNumber = parseInt(prompt('Your Guess: ')) //accepts users input
+    if(guessNumber === hiddenNo){ //compares input to the hiddenNo
+        stage++  //increase users stage if correct
+         console.log("You're in stage", stage, username)
+      console.log()
+      range++  //increase range if correct
       continue
     } else{
-      console.log('Incorrect Guess! Game Over',username)
-      break
+      console.log('Incorrect Guess! Game Over',username,'You stopped at stage', stage) 
+      break //ended loop if input is not equal to hiddenNo
     }
     
   }
